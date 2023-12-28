8930dc581af02a3266f6230a043c8a135d12dc90
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-12-28 12:53:34 +0100

    Handling Errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { get } from "./utils/http";
 import { BlogPost } from "./components/BlogPosts";
 import BlogPosts from "./components/BlogPosts";
 import image1 from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage";
 
 type BlogPostDataRaw = {
   userId: number;
@@ -16,38 +17,59 @@ type BlogPostDataRaw = {
 function App() {
 
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
+
 
   useEffect(() => {
+    setIsFetching(true);
     console.log('This runs only after the first render');
     async function fetchPosts() {
-      const data = (await get<BlogPostDataRaw[]>("https://jsonplaceholder.typicode.com/posts/")) as BlogPostDataRaw[];
 
-      const posts: BlogPost[] = data.map(post => {
-        return {
-          id: post.id,
-          title: post.title,
-          text: post.body
+      try {
+        const data = (await get<BlogPostDataRaw[]>("https://jsonplaceholder.typicode.com/posts/")) as BlogPostDataRaw[];
+        const posts: BlogPost[] = data.map(post => {
+          return {
+            id: post.id,
+            title: post.title,
+            text: post.body
+          }
+        });
+       
+        setFetchedPosts(posts);
+
+      } catch (error) {
+        if(error instanceof Error){
+          setError(error.message);
         }
-      });
-      setFetchedPosts(posts);
+      }
+      setIsFetching(false);
     }
 
     fetchPosts();
 
   }, []);
 
-  let content : React.ReactNode;
+  let content: React.ReactNode;
 
-  if(fetchedPosts){
+  if(error){
+    content = <ErrorMessage text={error} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Loading...</p>;
+  }
+
+  if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
   }
 
-  return(
+  return (
     <main>
-      <img src="{image1}" alt=""/>
+      <img src="{image1}" alt="" />
       {content}
     </main>
-  ); 
+  );
 }
 
 export default App;
